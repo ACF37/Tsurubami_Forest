b10b4a3cf86c2886e94c9e5ffb0fb23c7462f476
ACF37 <64457364+ACF37@users.noreply.github.com>
2024-02-15 04:49:09 +0000

    曲名コンポーネントを作る Fixes #1

---
+++ bami-forest/src/views/Song.vue
@@ -0,0 +1,13 @@
+<script setup>
+const props = defineProps({
+    song: {
+        type: Object,
+        required: true
+    }
+})
+</script>
+<template>
+    <div class="container">
+        {{ song.name }}
+    </div>
+</template>
